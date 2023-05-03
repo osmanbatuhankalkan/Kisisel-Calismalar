ed3c0047da9baa6a34dab167d8d39dc1e3136cb2
Osman Batuhan Kalkan <119897554+osmanbatuhankalkann@users.noreply.github.com>
2023-05-03 13:12:49 +0300

    Add files via upload

---
+++ Çarpım-Tablosu/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Denemeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Kendi Çarpım Tablomuzu Yapalım");
+            System.Console.WriteLine("İlk Değerimizi Girelim");
+            int a = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("İkinci Değerimizi Girelim");
+            int b = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Girilen Değerlerimiz : {0,0} {1,0} {2,0}",a,"ve",b);
+            
+
+            for(int i=a;i<=b;i++)
+            {   
+                
+                System.Console.WriteLine("{0,3} {1,0} {2,0} {3,0} {4,0}",i,"x",i,"=",i*i);
+                 }
+        }
+    }
+}
