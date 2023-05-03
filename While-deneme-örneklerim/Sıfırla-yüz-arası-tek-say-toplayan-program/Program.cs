4760211460e57a7d78f905ee4ae11489f638cdac
Osman Batuhan Kalkan <119897554+osmanbatuhankalkann@users.noreply.github.com>
2023-05-04 00:13:43 +0300

    Add files via upload

---
+++ While-deneme-örneklerim/Sıfırla-yüz-arası-tek-say-toplayan-program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace örnek
+{
+    class program
+    {
+        static void Main(string []args)
+        {
+            int artırıcıD=0,toplam=0;
+        
+            while(artırıcıD<100)
+            {   
+                if(artırıcıD%2==1)
+                {
+                    toplam +=artırıcıD;
+                }
+                artırıcıD++; 
+            }
+            System.Console.WriteLine(toplam);
+            Console.ReadKey();
+            
+        }
+    }
+}
