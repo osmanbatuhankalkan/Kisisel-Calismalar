2f69723ca83bb5bdfd5c533ff56be0b3c73a8db0
Osman Batuhan Kalkan <119897554+osmanbatuhankalkann@users.noreply.github.com>
2023-05-04 10:29:12 +0300

    Add files via upload

---
+++ While-deneme-örneklerim/Klavyeden_Girilen_Sayının_Faktöriyelini_While_ile_Alma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Deneme_Örnekleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Klavyeden girilen sayını faktöriyelini While ile alma
+            System.Console.WriteLine("Faktoriyelini almak istediğiniz değeri giriniz......");
+            int alınanSayı=int.Parse(Console.ReadLine());
+            int carpımToplam=1;
+            int faktoriyelDeger = alınanSayı;
+            
+            while(alınanSayı>=1)
+           {
+                 carpımToplam *=alınanSayı--;
+           } 
+           System.Console.WriteLine("{0,0}{1,0}{2,1}{3,1}",faktoriyelDeger,"!","=",carpımToplam);
+        }
+    }
+}
