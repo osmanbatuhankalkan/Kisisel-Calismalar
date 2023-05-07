5a87568aff746c7c5f6351102717fde60cce3689
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-05-07 18:54:18 +0300

    Add files via upload

---
+++ O_Anki_Tarihin_Saniye_Degeri_Besin_Katıysa_Yazdıran_Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Pratikler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bu for döngüsü ile de yapılabilir while ilede fakat fıtrat olarak for sayılabilen ardışık sayılara,while ise sonsuz döngülere daha uygun demiştik.
+            while(true)
+            {
+                if(DateTime.Now.Second %5 == 0)
+               {
+                System.Console.WriteLine(DateTime.Now);
+                
+               } 
+            }
+
+        }
+    }
+}
