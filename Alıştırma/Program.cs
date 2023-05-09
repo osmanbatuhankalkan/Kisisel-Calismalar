89fc096281101a47d96b15792de207c9c59988ee
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-05-09 23:57:17 +0300

    Add files via upload

---
+++ Alıştırma/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Alıstırma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Bölünecek sayıyı giriniz");
+            int a = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Bölen sayıyı giriniz");
+            int b = int.Parse(Console.ReadLine());
+
+            int c;
+
+            if(a%b==0)
+            {
+                System.Console.WriteLine("{0,0} {1,0} {2,0} sayısına",a,"tam bölünür",b);
+            }
+            else
+                System.Console.WriteLine("{0,0} {1,0} {2,0} sayısına",a,"tam bölünmez",b);
+        } 
+    }
+}
