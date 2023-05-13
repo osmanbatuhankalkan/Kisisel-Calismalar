46ced0aae17b28c89e27927e8e570d25946b4d22
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-05-13 14:51:56 +0300

    Add files via upload

---
+++ Dizi-Döngü-Length/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Dizi_Döngü_Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[]personeller=new string[18];
+            personeller[0]="ali";
+            personeller[1]="ayse";
+            personeller[2]="fatma";
+            personeller[3]="veli";
+            personeller[4]="mehmet";
+            personeller[5]="ahmet";
+            personeller[6]="can";
+            personeller[7]="ece";
+            personeller[8]="ezgi";
+            personeller[9]="şenol";
+
+            for(int i=0;i<personeller.Length;i++)
+            {
+                System.Console.WriteLine(personeller[i]);
+            }
+            System.Console.WriteLine("Bellekte Rezerv Şekilde Bekleyen Default Değerler");
+        }
+    }
+}
