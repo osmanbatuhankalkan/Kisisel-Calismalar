20c4f4b7893e642d285b9f65af3d17faf3793519
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-05-29 23:58:15 +0300

    Add files via upload

---
+++ Array_Sınıfı_Clear/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Person
+{
+class program
+        {
+    static void Main(String[] args) 
+    {
+        //CLEAR
+        Array isimler = new[]{"Hilmi","Hüseyin","ALi","Veli","Tan"};
+        /*Dizi içerisindeki tüm elemanlar siliniyor olarak bilinir Fakat bu yanlış bir bilgidir.
+        Dizi içerisindeki tüm elemanlara, dizi türüne uygun default değerleri atayan bir Fonksiyondur.  */
+        for(int i =0;i<isimler.Length;i++)
+        {
+            System.Console.WriteLine(isimler.GetValue(i));
+        }
+        Array.Clear(isimler,1,2);
+        System.Console.WriteLine("*******************************");
+        for(int i=0;i<isimler.Length;i++)
+        {
+            System.Console.WriteLine(isimler.GetValue(i));
+        }
+            
+    }
+
+    }
+}
