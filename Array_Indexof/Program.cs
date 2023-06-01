ca08be60657976b6e8f245231acb8ffeed2da52e
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-06-01 23:42:28 +0300

    Add files via upload

---
+++ Array_Indexof/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Array_Indexof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DİZİ İÇERİSİNDE BİR ELEMANIN VAR OLUP OLMADIĞINI SORGULAYABİLDİĞİMİZ FONKSİYONDUR
+            Array isimler = new[]{"ali","veli","cengiz","ahmet","mehmet"};
+            
+            int index = Array.IndexOf(isimler,"veli");//Arama neticesinde ilgili değer varsa int olarak o değeri index numarasını döndürecektir
+            //YOK İSE -1 olarak döndürür
+            if(index != -1)
+            {
+                //Demek ki aranan değer ilgili dizide bulunmaktadır....
+                System.Console.WriteLine("var");
+            }
+            int index2 = Array.IndexOf(isimler,"ahmet",0,4);//isimlerin içinde ahmet 0.index ile 4. index arasında ara demek
+
+            if(index2 != -1)
+            {
+                //Demek ki aranan değer ilgili dizide bulunmaktadır....
+                System.Console.WriteLine("var");
+            }     
+            }
+    }
+}
