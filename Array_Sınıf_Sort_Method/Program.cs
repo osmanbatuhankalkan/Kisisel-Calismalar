5935fa3265bedf279f1018abe3526cb3d2e8ece0
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-06-04 23:57:55 +0300

    Add files via upload

---
+++ Array_Sınıf_Sort_Method/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Array_Indexof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array isimler = new[]{"hilmi","hüseyin","ali","veli","ahmet"};
+            for(int i=0;i<isimler.Length;i++)
+            {
+                System.Console.WriteLine(isimler.GetValue(i));
+
+                Array.Sort(isimler);
+                System.Console.WriteLine("***************");
+
+                for(int a=0;a<isimler.Length;a++)
+                {
+                    System.Console.WriteLine(isimler.GetValue(i));
+                }
+            }
+        }
+    }
+}
